fa7f18a317d36201fc7a802b62f50f2cbe675fda
Jaydev Thomke <jaydev.thomke@firstcry.com>
2020-03-30 15:18:03 +0530

    Stream api

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ const config = {
 let count = 0;
 const client = new Client(config)
 client.connect()
+const QueryStream = require('pg-query-stream')
+const JSONStream = require('JSONStream')
+const request = require('request');
+
+//pipe 1,000,000 rows to stdout without blowing up your memory usage
 // client.query('SELECT NOW()', (err, res) => {
 //   console.log(err, res)
 //   client.end()
@@ -27,10 +32,39 @@ const insert = () => {
   })
 };
 
-insert();
+// insert();
 
-app.get('/', (req, res) => {
+app.get('/:limit', (req, res) => {
+  const queryString = 'SELECT * FROM books' + (req.params.limit ? (' limit ' + req.params.limit) : '');
+  console.log(queryString);
+  const query = new QueryStream(queryString);
+  const stream = client.query(query);
+  let array = '';
+  // release the client when the stream is finished
+  stream.on('end', () => {
+    console.log('ended', array);
+  })
+  stream.on('data', (data) => {
+    console.log('data', data);
+    array += JSON.stringify(data);
+
+  })
+  stream.pipe(JSONStream.stringify()).pipe(res);
 })
 
+
+app.get('/getdata/:limit', (req, res) => {
+
+  const stream = request('http://localhost:3000/' + req.params.limit);
+
+  stream.on('end', () => {
+    console.log('ended');
+  })
+  stream.on('data', (data) => {
+    console.log('data', data.toString());
+    // array += JSON.stringify(data);
+  })
+  stream.pipe(res);
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
